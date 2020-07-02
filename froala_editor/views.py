9c031033f3e51fd6c4cc92bfed0821a775b2efc7
kdfwow64 <kdfwow64@gmail.com>
2020-07-02 14:28:32 +0800

    fix

--- froala_editor/views.py
+++ froala_editor/views.py
@@ -6,6 +6,8 @@ import os
 from django.utils.translation import ugettext_lazy as _
 from django.utils.module_loading import import_string
 
+import uuid
+
 
 # Allow for a custom storage backend defined in settings.
 def get_storage_class():
@@ -13,6 +15,11 @@ def get_storage_class():
 
 
 storage = get_storage_class()
+
+def unique_filename(name):
+    ext = name.split('.')[-1]
+    filename = "%s.%s" % (name.replace('.' + ext, '') + '-' + str(uuid.uuid4()), ext)
+    return filename
 
 
 def image_upload(request):
@@ -33,9 +40,20 @@ def image_upload(request):
         # filesize = len(file['content'])
         # filetype = file['content-type']
 
-        upload_to = getattr(settings, 'FROALA_UPLOAD_PATH', 'uploads/froala_editor/images/')
-        path = storage.save(os.path.join(upload_to, the_file.name), the_file)
-        link = request.build_absolute_uri(storage.url(path))
+        upload_to = getattr(settings, 'FROALA_UPLOAD_PATH', 'uploads/froala_editor/')
+        upload_to = upload_to + 'images/'
+
+        path = storage.save(os.path.join(upload_to, unique_filename(the_file.name)), the_file)
+        # link = request.build_absolute_uri(storage.url(path))
+
+        link = 'https://' + getattr(settings, 'AWS_STORAGE_BUCKET_NAME', 'sengkuyung-dev') + '.s3-' + getattr(settings, 'AWS_S3_REGION_NAME',
+                                                                                                              'ap-southeast-1') + '.amazonaws.com' + path
+        # cloudfront_domain = getattr(settings, 'CLOUDFRONT_DOMAIN', '')
+        # if cloudfront_domain == '':
+        #     link = 'https://' + getattr(settings, 'AWS_STORAGE_BUCKET_NAME', 'sengkuyung-dev') + '.s3-' + getattr(settings, 'AWS_S3_REGION_NAME',
+        #                                                                                                           'ap-southeast-1') + '.amazonaws.com' + path
+        # else:
+        #     link = 'https://' + cloudfront_domain + path
 
         # return JsonResponse({'link': link})
         return HttpResponse(json.dumps({'link': link}), content_type="application/json")
@@ -44,7 +62,11 @@ def image_upload(request):
 def file_upload(request):
     if 'file' in request.FILES:
         the_file = request.FILES['file']
-        upload_to = getattr(settings, 'FROALA_UPLOAD_PATH', 'uploads/froala_editor/files/')
-        path = storage.save(os.path.join(upload_to, the_file.name), the_file)
-        link = storage.url(path)
+        upload_to = getattr(settings, 'FROALA_UPLOAD_PATH', 'uploads/froala_editor/')
+        upload_to = upload_to + 'files/'
+
+        path = storage.save(os.path.join(upload_to, unique_filename(the_file.name)), the_file)
+        # link = storage.url(path)
+        link = 'https://' + getattr(settings, 'AWS_STORAGE_BUCKET_NAME', 'sengkuyung-dev') + '.s3-' + getattr(settings, 'AWS_S3_REGION_NAME',
+                                                                                                              'ap-southeast-1') + '.amazonaws.com' + path
         return HttpResponse(json.dumps({'link': link}), content_type="application/json")
